6675bee15ac165fb83b75b94152fd065500749c6
Dale Fielding <dalefielding28@gmail.com>
2024-07-02 19:06:51 +0000

    Feat: add grapes of passion app

---
+++ grapes_of_passion/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for grapes_of_passion project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grapes_of_passion.settings')
+
+application = get_wsgi_application()
