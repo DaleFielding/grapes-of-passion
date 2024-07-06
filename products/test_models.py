11e84e4055dd42844374be86b9674525f3355863
Dale Fielding <dalefielding28@gmail.com>
2024-07-06 15:14:25 +0000

    Feat: add test_category_creation - first test fails

---
+++ products/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from .models import Category, Product
+
+
+class CategoryModelTest(TestCase):
+    # Testing if a category can be created and with the appropriate fields.
+    def test_category_creation(self):
+        category = Category.objects.create(
+            name='Intentionally incorrect name',
+            friendly_name='Expected friendly name'
+        )
+        # Checking if category was created with the expected fields
+        self.assertEqual(category.name, 'Expected name')
+        self.assertEqual(category.friendly_name, 'Expected friendly name')
