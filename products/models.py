71aeceab429064e334c48dcb85efd89a937f242d
Dale Fielding <dalefielding28@gmail.com>
2024-07-06 13:45:36 +0000

    Feat: add products model

--- products/models.py
+++ products/models.py
@@ -10,3 +10,25 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+class Product(models.Model):
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    discount = models.CharField(max_length=50, null=True, blank=True)
+    discounted_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    grape = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    unit_volume = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True)
+    type = models.CharField(max_length=50, null=True, blank=True)
+    alcohol_percentage = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    region_state = models.CharField(max_length=100, null=True, blank=True)
+    style = models.CharField(max_length=100, null=True, blank=True)
+    is_vegetarian = models.BooleanField(default=False)
+    is_vegan = models.BooleanField(default=False)
+    image = models.ImageField(null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
