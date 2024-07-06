0499e937b3f337a2dda1b2c3756389db46cbb0ee
Dale Fielding <dalefielding28@gmail.com>
2024-07-06 15:23:15 +0000

    Test: make created friendly name different than expected - second test fails

--- products/test_models.py
+++ products/test_models.py
@@ -6,8 +6,8 @@ class CategoryModelTest(TestCase):
     # Testing if a category can be created and with the appropriate fields.
     def test_category_creation(self):
         category = Category.objects.create(
-            name='Intentionally incorrect name',
-            friendly_name='Expected friendly name'
+            name='Expected name',
+            friendly_name='Intentionally incorrect friendly name'
         )
         # Checking if category was created with the expected fields
         self.assertEqual(category.name, 'Expected name')
