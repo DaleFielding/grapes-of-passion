04bd877b3e006b04c74e7ae27092807256929ba1
Dale Fielding <dalefielding28@gmail.com>
2024-07-07 11:27:34 +0000

    Feat: add test_product_detail_view - 1st test error (no view, function or pattern name)

--- products/test_views.py
+++ products/test_views.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
 from django.urls import reverse
-
+from .models import Product
 
 class TestViews(TestCase):
+    # Test product view
     def test_products_view(self):
         response = self.client.get(reverse('products'))  
         self.assertEqual(response.status_code, 200)  
         self.assertTemplateUsed(response, 'products/products.html') 
+
+    # Load fixtures for the below test
+    fixtures = ['categories.json', 'products.json']  
+
+    # Test product detail view
+    def test_product_detail_view(self):
+        product = Product.objects.get(pk=3)
+        url = reverse('product_detail', args=[product.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/product_detail.html')
+        self.assertContains(response, product.name)
+
