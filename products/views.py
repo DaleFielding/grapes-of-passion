810f957c0363169fe40143ee7a10959f93796c04
Dale Fielding <dalefielding28@gmail.com>
2024-07-08 07:28:32 +0000

    Feat: include method to group categories and amend method to display these categories

--- products/views.py
+++ products/views.py
@@ -1,36 +1,52 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
+# Create your views here.
 
 def all_products(request):
     """ 
     Handle the display of all wine products,
     including sorting and search queries 
     """
-    # Get all products and set query to None initially to prevent query error
-    # Print request.GET for debugging purposes
-    print(request.GET)
     
+    # Get all products and assign query/category to None to prevent errors
     products = Product.objects.all()
     query = None
-    # Check if a query searched.
-    # If not show error message and redirect to products page.
+    categories = None
+
+    # Mapping to group certain categories
+    grouped_categories = {
+        'all-wine': ['red', 'white', 'rose'],
+        'all-fortified-wine': ['port', 'sherry']
+    }
+
+    # If request contains category, filter products by category.
+    # Handle grouped categories if specified in query parameters
     if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category']
+            if category in grouped_categories:
+                categories = grouped_categories[category]
+            else:
+                categories = [category]
+                
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # Check if a query searched.
+        # If not show error message and redirect to products page.
         if 'query' in request.GET:
             query = request.GET['query']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
+                
             # Adjust queries to search for name or type.
-            queries = Q(name__icontains=query) | Q(type__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-        # Check if category_ids are included in the get request,
-        # then split the id's and filter the products by category_id
-        if 'category_ids' in request.GET:
-            category_ids = request.GET['category_ids'].split(',')
-            products = products.filter(category_id__in=category_ids)
+
         # If the discounted parameter is in the get request,
         # filter by including products that are not discounted
         if 'discounted' in request.GET:
@@ -40,8 +56,11 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
+
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
