8a80696962fd7b634f23b41c2bba674970b6e8e9
Dale Fielding <dalefielding28@gmail.com>
2024-07-09 18:31:51 +0000

    Fix: way of displaying products/title in products page, functionality now handled in the view, and simplified django templating.

--- products/views.py
+++ products/views.py
@@ -3,40 +3,43 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
-# Create your views here.
 
+# ---------- ALL PRODUCTS ---------- 
 def all_products(request):
     """ 
     Handle the display of all wine products,
-    including sorting and search queries 
+    including sorting and search queries
     """
     
-    # Get all products and assign query/category to None to prevent errors
+    # Get all products and set default values for variables
     products = Product.objects.all()
     query = None
     categories = None
     is_discounted = False
+    display_title = "No Products Found" 
 
     # Mapping to group certain categories
     grouped_categories = {
         'all-wine': ['red', 'white', 'rose'],
         'all-fortified-wine': ['port', 'sherry']
     }
-
-    # If request contains category, filter products by category.
-    # Handle grouped categories if specified in query parameters
+# When get request contains 'category', filter products by category.
+# Handle grouped categories if specified in query parameters
+# Then assign intended title to display_title
     if request.GET:
         if 'category' in request.GET:
             category = request.GET['category']
             if category in grouped_categories:
                 categories = grouped_categories[category]
+                display_title = "All Wine" if category == 'all-wine' else "All Fortified Wine"
             else:
                 categories = [category]
+                display_title = Category.objects.get(name=category).name.capitalize()
                 
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-        # Check if a query searched.
+        # Handle query searched.
         # If not show error message and redirect to products page.
         if 'query' in request.GET:
             query = request.GET['query']
@@ -44,26 +47,33 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
                 
-            # Adjust queries to search for name or type.
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            # Filter products by name or type)
+            # Then assign query to display_title
+            queries = Q(name__icontains=query) | Q(type__icontains=query)
             products = products.filter(queries)
+            display_title = f"Results for '{query}'" 
 
         # If the discounted parameter is in the get request,
         # filter by including products that are not discounted
+        # Then assign intended title to display_title
         if 'discounted' in request.GET:
-            is_discounted = request.GET['discounted']
+            is_discounted = True
             products = products.exclude(discounted_price__isnull=True)
+            display_title = "Discounted Products"  
 
+    # context variables to be passed to the template
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'is_discounted': is_discounted,
+        'display_title': display_title,
     }
 
     return render(request, 'products/products.html', context)
 
 
+# ---------- PRODUCT DETAILS ---------- 
 def product_detail(request, product_id):
     """ A view to show individual product details """
     product = get_object_or_404(Product, pk=product_id)
