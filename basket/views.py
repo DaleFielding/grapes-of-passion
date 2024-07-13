e429ca55817f455a7a211be6f5b50841ccfae2b4
Dale Fielding <dalefielding28@gmail.com>
2024-07-13 13:27:08 +0000

    Feat: add views for adjusting and removing items from the basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -24,3 +24,28 @@ def add_to_basket(request, item_id):
     request.session['basket'] = basket
     print(request.session['basket'])
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product in the shopping basket"""
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if item_id in basket and quantity > 0:
+        basket[item_id] = quantity
+    elif item_id in basket and quantity <= 0:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping basket"""
+    basket = request.session.get('basket', {})
+
+    if item_id in basket:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return HttpResponse(status=200)
