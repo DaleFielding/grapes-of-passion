6dfb385d46fe270bc8cfc3576feabd7edd954001
Dale Fielding <dalefielding28@gmail.com>
2024-07-13 18:34:09 +0000

    Feat: add message_storage to settings for use with gitpod

--- grapes_of_passion/settings.py
+++ grapes_of_passion/settings.py
@@ -86,6 +86,11 @@ TEMPLATES = [
     },
 ]
 
+
+# Required due to the use of GitPod
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
