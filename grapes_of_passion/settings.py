02f681b4840d2ef8a5a08624e096b354edea0958
Dale Fielding <dalefielding28@gmail.com>
2024-07-14 12:00:18 +0000

    Feat: add basic stripe checkout functionality

--- grapes_of_passion/settings.py
+++ grapes_of_passion/settings.py
@@ -170,8 +170,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Free/charged delivery
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+# Stripe
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
