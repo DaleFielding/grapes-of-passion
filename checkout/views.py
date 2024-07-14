65d9fc6711434c24fd4497da798ec7f73647af37
Dale Fielding <dalefielding28@gmail.com>
2024-07-14 11:31:11 +0000

    Feat: add stripe elements, including script and styling

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51POC3v2MrHAQw8dWaSIXgSo3g8fRKgB9awgmc7DdiLrZh5WEXhK6f5btCHi3GtptsKSo6y7nQzhpmv2dYSrFuBuo00cO9jzMjH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
