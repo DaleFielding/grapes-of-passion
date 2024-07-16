e51e21fbef99e8a08512a8d11681d1779fea35f5
Dale Fielding <dalefielding28@gmail.com>
2024-07-16 18:45:50 +0000

    Fix: add functinality to get UserProfile instance within profile view

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,8 @@ from .models import UserProfile
 
 def profile(request):
     """ Display the user's profile. """
-
+    profile = get_object_or_404(UserProfile, user=request.user)
+    
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
