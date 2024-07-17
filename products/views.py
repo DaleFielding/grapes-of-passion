ba1cefd13193dc70bde0f94b9bc5906a78d59684
Dale Fielding <dalefielding28@gmail.com>
2024-07-17 19:13:11 +0000

    Feat: add functionality to handle form submission and feedback through messaging

--- products/views.py
+++ products/views.py
@@ -108,10 +108,22 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
-# ---------- ADD PRODUCT ----------
+# ---------- ADD PRODUCT: ADMIN ----------
 def add_product(request):
-    """ Add a product to the store """
-    form = ProductForm()
+    """ Add a product to the admin store """
+
+    # Handle form submission and feedback through messaging
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
