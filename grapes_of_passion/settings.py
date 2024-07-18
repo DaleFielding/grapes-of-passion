9d9b7f5cc9db421386f87dffd64ed2168e0309d0
Dale Fielding <dalefielding28@gmail.com>
2024-07-18 19:07:07 +0000

    Feat: update secret key to environment variable and set debug

--- grapes_of_passion/settings.py
+++ grapes_of_passion/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rq%6a3nq=5-91%-$ly9%co&(r!_i&ontrd(4xssdt%gaj1*8*!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['grapes-of-passion-37a9373e50d5.herokuapp.com',
 '8000-dalefieldin-grapesofpas-c7sa90tsxvm.ws.codeinstitute-ide.net',
