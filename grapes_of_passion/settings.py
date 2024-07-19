9c01be3acd303d507b166babb909cf035bbfab4a
Dale Fielding <dalefielding28@gmail.com>
2024-07-19 06:00:27 +0000

    Feat: Add settings to handle bucket, static, media files and overrides for when USE_AWS is in envrion

--- grapes_of_passion/settings.py
+++ grapes_of_passion/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'profiles',
     # Other
     'crispy_forms',
+    'storages'
 
     
 ]
@@ -200,9 +201,29 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'grapes-of-passion'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
 # Trusted Origins
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-dalefieldin-grapesofpas-c7sa90tsxvm.ws.codeinstitute-ide.net'
+    'https://8000-dalefieldin-grapesofpas-c7sa90tsxvm.ws.codeinstitute-ide.net',
+    'https://grapes-of-passion-37a9373e50d5.herokuapp.com/'
     ]
 
 # Other
