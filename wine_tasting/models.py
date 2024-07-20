8cb816f50aa62b0dd1c838be4938e19ab2d29f13
Dale Fielding <dalefielding28@gmail.com>
2024-07-20 20:01:28 +0000

    Fix: remove category from model as is is causing unecessary complications

--- wine_tasting/models.py
+++ wine_tasting/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class WineTastingProduct(models.Model):
+    product_name = models.CharField(max_length=100, null=False)
+    sku = models.CharField(max_length=100, unique=True, null=False)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dates = models.TextField()
+
+    def __str__(self):
+        return self.product_name
