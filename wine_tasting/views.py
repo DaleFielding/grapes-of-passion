d89ab1e08d37fb903aca680ef710ecc9fcf2cd8e
Dale Fielding <dalefielding28@gmail.com>
2024-07-21 10:05:28 +0000

    Feat: update wine_tasting view to handle form validation and pass data to the template

--- wine_tasting/views.py
+++ wine_tasting/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import WineTastingProduct
+from .forms import WineTastingForm
+import json
 
 
 def wine_tasting(request):
+    if request.method == 'POST':
+        form = WineTastingForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return redirect('wine_tasting')  
+    else:
+        form = WineTastingForm()
 
-    return render(request, 'wine_tasting/wine_tasting.html')
+    wine_tastings = WineTastingProduct.objects.all()
+    dates_dict = {str(product.id): product.dates.split(',') for product in wine_tastings}
+
+    return render(request, 'wine_tasting/wine_tasting.html', {
+        'WineTastingProducts': wine_tastings,
+        'form': form,
+        'dates_json': json.dumps(dates_dict)
+    })
