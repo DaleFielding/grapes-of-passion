ceaeec0bddb04569c5286e11acaed1a145a131ec
Dale Fielding <dalefielding28@gmail.com>
2024-07-22 19:31:16 +0000

    Feat: update wine_tasting view to include limits for the quantity field

--- wine_tasting/views.py
+++ wine_tasting/views.py
@@ -15,11 +15,19 @@ def wine_tasting(request):
 
     wine_tastings = WineTastingProduct.objects.all()
     dates_dict = {str(product.id): product.dates.split(',') for product in wine_tastings}
+    experiences_info = {
+        str(product.id): {
+            'name': product.product_name,
+            'min_quantity': 2 if 'couple' in product.product_name.lower() else 3,
+            'max_quantity': 2 if 'couple' in product.product_name.lower() else 12
+        } for product in wine_tastings
+    }
     price = wine_tastings.first().price if wine_tastings else 0
 
     return render(request, 'wine_tasting/wine_tasting.html', {
         'WineTastingProducts': wine_tastings,
         'form': form,
         'dates_json': json.dumps(dates_dict),
+        'experiences_info_json': json.dumps(experiences_info),
         'price': price
     })
