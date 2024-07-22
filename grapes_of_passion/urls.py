a3cc9a9259997d26ba2c18441c95098b46d2247f
Dale Fielding <dalefielding28@gmail.com>
2024-07-22 09:09:30 +0000

    Feat: add 404 page

--- grapes_of_passion/urls.py
+++ grapes_of_passion/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wine_tasting/', include('wine_tasting.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'grapes_of_passion.views.custom_404'
