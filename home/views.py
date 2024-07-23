8f8f461208d9e7cc79049652b5d41ed32e2ccddc
Dale Fielding <dalefielding28@gmail.com>
2024-07-23 19:55:33 +0000

    Fix: add logic to replace product with a random product if one of the specified products is deleted

--- home/views.py
+++ home/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-from products.models import Product 
+from products.models import Product  
+
+
+def get_random_product(exclude_pks):
+    """
+    Get random product, excluding products with specified primary keys
+    """
+    products = Product.objects.exclude(pk__in=exclude_pks)
+    if not products.exists():
+        return None  
+    return products.order_by('?').first()
 
 
 def index(request):
-    """ A view to return the index page """
+    """
+    Render index page with specified products or random replacements.
+    """
+    product_pks = [1, 4, 7, 24]
+    products = []
 
-    # Recommended products to be displayed
-    product1 = get_object_or_404(Product, pk=1)
-    product2 = get_object_or_404(Product, pk=4)
-    product3 = get_object_or_404(Product, pk=7)
-    product4 = get_object_or_404(Product, pk=24)
+    for pk in product_pks:
+        try:
+            product = get_object_or_404(Product, pk=pk)
+        except:
+            product = get_random_product([p.pk for p in products])
+            if not product:
+                continue
+        products.append(product)
 
     context = {
-        'products': [product1, product2, product3, product4],
+        'products': products,
     }
     return render(request, 'home/index.html', context)
