97040a14cba24a0c566602d04512bcbd9aeb4541
Dale Fielding <dalefielding28@gmail.com>
2024-07-24 17:41:11 +0000

    Fix: update field names to match the model

--- wine_tasting/forms.py
+++ wine_tasting/forms.py
@@ -1,27 +1,34 @@
 from django import forms
-from .models import WineTastingProduct
-
+from .models import WineTastingBooking, WineTastingProduct
 
 class WineTastingForm(forms.ModelForm):
     class Meta:
-        model = WineTastingProduct
-        fields = ['experience', 'contact_number', 'special_requirements', 'date', 'quantity']
+        model = WineTastingBooking
+        fields = ['wine_tasting_product', 'contact_number', 'special_requirements', 'selected_date', 'number_of_people']
         labels = {
-            'experience': '',
+            'wine_tasting_product': '',
             'contact_number': '',
             'special_requirements': '',
-            'date': '',
-            'quantity': ''
+            'selected_date': '',
+            'number_of_people': ''
         }
 
-    experience = forms.ChoiceField(required=True)
+    wine_tasting_product = forms.ChoiceField(required=True)
     contact_number = forms.CharField(max_length=15, required=True)
     special_requirements = forms.CharField(required=False)
-    date = forms.CharField(required=True)
-    quantity = forms.IntegerField(min_value=1, required=True)
+    selected_date = forms.DateTimeField(required=True)
+    number_of_people = forms.IntegerField(min_value=1, required=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['experience'].choices = [('', '-Experiences-')] + [
+        self.fields['wine_tasting_product'].choices = [('', '-Experiences-')] + [
             (product.id, product.product_name) for product in WineTastingProduct.objects.all()
         ]
+    
+    def clean_wine_tasting_product(self):
+        product_id = self.cleaned_data['wine_tasting_product']
+        try:
+            product = WineTastingProduct.objects.get(id=product_id)
+        except WineTastingProduct.DoesNotExist:
+            raise forms.ValidationError("Invalid wine tasting product selected.")
+        return product
